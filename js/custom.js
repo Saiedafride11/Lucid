3ef92e1c8e7743d2fa4928a4068772a637191c78
Khalid Shaifullah <56803186+Saiedafride11@users.noreply.github.com>
2021-01-08 06:02:42 -0800

    Edit

---
+++ js/custom.js
@@ -0,0 +1,18 @@
+
+//stick.js plug in code goes here
+//initiating jQuery
+jQuery(function($) {
+    $(document).ready( function() {
+        //enabling stickUp on the '.navbar-wrapper' class
+        $('.navbar-wrapper').stickUp();
+    });
+    });
+
+
+//wow.js plug in code goes here
+//initiating jQuery
+jQuery(function($) {
+    $(document).ready(function(){
+       new WOW().init();
+   });
+   });
